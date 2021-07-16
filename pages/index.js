c00dc596b0d66ff8b553c829c8470928c1f0e139
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-07-16 14:28:49 -0300

    ultimo dia

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -37,9 +39,8 @@ function ProfileRelationsBox(propriedades) {
     </ProfileRelationsBoxWrapper>
   )
 }
-export default function Home() {
-  
-  const usuarioAleatorio = 'alewssandra';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'juunegreiros',
@@ -53,6 +54,7 @@ export default function Home() {
   const [seguidores, setSeguidores] = React.useState([]);
   // 0 - Pegar o array de dados do github 
   React.useEffect(function() {
+    //get
     fetch('https://api.github.com/users/alewssandra/followers')
     .then(function (respostaDoServidor) {
       return respostaDoServidor.json();
@@ -201,4 +203,31 @@ console.log('seguidores antes do return', seguidores);
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+        Authorization: token
+      }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
 }
